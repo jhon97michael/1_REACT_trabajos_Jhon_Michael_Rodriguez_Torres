a2eb4d9a77d6b90f71a0b0a86cf447c00ef99465
Maintrobot <112593253+jhon97michael@users.noreply.github.com>
2023-09-16 13:14:47 -0500

    actualizacion y funciones js
    Se realizar funciones y eventos en JS

--- REACT/js/index.js
+++ REACT/js/index.js
@@ -1,4 +1,38 @@
-const aplicacion = document.querySelector('.react');
-console.log(aplicacion);
-const h2 =document.getElementById('#diferencia');
-console.log(h2);
+const tvreact = document.querySelector(".react");
+
+function freact(e){
+    console.log("React");
+}
+
+tvreact.addEventListener("click", freact);
+
+const angular = document.querySelector(".angular");
+
+function fangular(e){  
+    console.log("Angular");
+}
+
+angular.addEventListener("click", fangular);
+
+const tvVue = document.querySelector(".vue");
+
+function fvue(e){
+    console.log("Vue");
+}
+
+tvVue.addEventListener("click", fvue);
+
+const semejanzasElement = document.querySelector(".semejanzas");
+
+function handleMousemove(e) {
+    console.log("semejanzas");
+}
+
+semejanzasElement.addEventListener("mousemove", handleMousemove);
+
+const btinicio = document.querySelector(".getStart")
+
+function blurbtn(e){
+    console.log("Blur Boton");
+}
+btinicio.addEventListener("onblur", blurbtn);
