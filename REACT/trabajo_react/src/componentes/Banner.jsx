4db652cb0ac37ab5e27cdb5fe062f14973e4c184
jhon97michael <112593253+jhon97michael@users.noreply.github.com>
2023-10-19 11:33:56 -0500

    Se actualiza
    Se actualiza los componentes de banner y barra de navegacion

--- REACT/trabajo_react/src/componentes/Banner.jsx
+++ REACT/trabajo_react/src/componentes/Banner.jsx
@@ -3,14 +3,16 @@ import '../Estilos/Banner.css'
 
 function banner(){
     return(
-        <div className='contenedor-baner'>
+        <section id='hero'>
+        <div className='contenedor-hero'>
             <div class="parent">
-                <div class="div1">ANGULAR </div>
-                <div class="div2">REACT </div>
-                <div class="div3">VUE </div>
+                <div class="div1"><h2>ANGULAR</h2> </div>
+                <div class="div2"><h2>REACT</h2> </div>
+                <div class="div3"><h2>VUE</h2> </div>
                 <div class="div4">Explorando las </div>
                 <div class="div5">Definiciones, diferencias y semejanzas. </div>
             </div>
         </div>
+        </section>
     );
 }export default banner;
